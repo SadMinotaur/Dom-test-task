d6119213f705182165c8c29128c3741eed78a2b9
Dima Balandin <dimabalandin@Dimas-MacBook-Air.local>
2023-02-19 12:18:58 +0600

    init commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import RootScreen from './src/RootSceeen';
+
+export default function App(): JSX.Element {
+  return (
+    <SafeAreaView style={{}}>
+      <RootScreen />
+    </SafeAreaView>
+  );
+}
